d8acf50b60ed45f6bec7b2b761d703f576f45272
yeelimtse <yix176@ucsd.edu>
2018-10-04 17:51:50 -0700

    index update

--- source/react/src/index.js
+++ source/react/src/index.js
@@ -10,14 +10,5 @@ ReactDOM.render(
 <App />
 </Router>,
 
-<<<<<<< HEAD:cse-110-proj/src/index.js
 document.getElementById('root'));
 registerServiceWorker();
-=======
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-//try to push
->>>>>>> c69d908a8994da0f22b4b10ccb8e2f06d2adde78:source/react/src/index.js
