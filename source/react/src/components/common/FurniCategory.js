81fc5cfa1cefb5c58cefe1fb7f3b173855fd1811
yeelimtse <yix176@ucsd.edu>
2018-10-31 02:27:58 -0700

    FurniCategory-Initialized

---
+++ source/react/src/components/common/FurniCategory.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import FurniCard from './FurniCard';
+import './FurniCategory.css';
+
+
+class FurniCategory extends React.Component {
+    // states initialization
+
+    // functions
+
+    // render 
+    render() {
+        return (
+            <div className="category-container">
+            
+            </div>
+        );
+    }
+}
+
+FurniCategory.protoTypes = {
+    classes: protoTypes.object.isRequired,
+};
