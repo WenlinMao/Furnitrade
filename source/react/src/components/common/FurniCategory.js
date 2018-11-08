35707e260a9f6775aadfac7e09c727a7bf48ec9b
yeelimtse <yix176@ucsd.edu>
2018-11-08 12:43:23 -0800

    FurniCategory_Initialization

--- source/react/src/components/common/FurniCategory.js
+++ source/react/src/components/common/FurniCategory.js
@@ -13,7 +13,29 @@ class FurniCategory extends React.Component {
     render() {
         return (
             <div className="category-container">
-            
+                    <h2>My Works</h2>
+                <div className="category-group">
+                <FurniCard 
+                    title="cate1" 
+                    text="description for category1"
+                    image={require('../../static/images/test-logo.jpg')}
+                />
+                <FurniCard 
+                    title="cate2" 
+                    text="description for category2"
+                    image={require('../../static/images/test-logo.jpg')}
+                />
+                <FurniCard 
+                    title="cate3" 
+                    text="description for category3"
+                    image={require('../../static/images/test-logo.jpg')}
+                />
+                <FurniCard 
+                    title="cate4" 
+                    text="description1 for category4"
+                    image={require('../../static/images/test-logo.jpg')}
+                />
+                </div>
             </div>
         );
     }
