ed99aa53c8c81d7ed63eab3c48ace4b1b21eb3c0
yeelimtse <yix176@ucsd.edu>
2018-11-08 12:34:12 -0800

    card-component-finished

--- source/react/src/components/common/FurniCard.js
+++ source/react/src/components/common/FurniCard.js
@@ -1,58 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import React from 'react'
+import './FurniCard.css'
 
-const styles = {
+/* Furniture card compoenent
+- image: the image of furniture posted
+- title: name of the furniture
+- text: description of the furniture (could be replaced by a button)
+*/
 
-    card: {
-        maxWidth: 300,
-    },
-    media: {
-        height: 200,
-    },
 
-};
+const Card = props => (
+    <div className="Card">
+        <img src={props.image} />
+        <h3>{props.title}</h3>
+        <p>{props.text}</p>
+    </div>
+)
 
-function FurniCard(props){
-    const {classes} = props;
-    const imgPath = props.imagePath;
-    
-    return(
-        <Card className={classes.card}>
-            
-            {/* When creating Furnicard components, please specify the image by passing imagePath = ??? */}
-            <CardMedia 
-                component = "img" 
-                className={classes.media} 
-                image = {props.imagePath}
-            />
-
-            {/* When creating Furnicard components, please specify the name by passing furniName = ??? */}
-            <CardContent>
-                <Typography gutterBottom variant="headline" component="h2">
-                    {props.furniName}
-                </Typography>
-            </CardContent>
-          
-            {/* The user shall be able to send an info form to sellers through this button */}
-            <CardActions>
-            <Button  size="small" color="primary">
-                I want this
-            </Button>
-            </CardActions>
-      </Card>
-    );
-}
-
-FurniCard.propTypes = {
-    classes: PropTypes.object.isRequired,
-
-  };
-  
-  export default withStyles(styles)(FurniCard);
+export default Card
