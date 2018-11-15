c1915ce9e350b840077e743386683333019ad914
Yilin Cai <yilincai@YilindeMacBook-Pro.local>
2018-11-14 21:24:48 -0800

    add furniture detail

--- source/flask/flaskr/contact_form.py
+++ source/flask/flaskr/contact_form.py
@@ -2,40 +2,49 @@ from functools import wraps
 from flask_restful import Api, Resource, url_for
 
 from flask import (
-	Blueprint, flash, g, redirect, render_template, request, session, url_for,
-	jsonify
+    Blueprint, flash, g, redirect, render_template, request, session, url_for,
+    jsonify
 )
 
-########### Additional Dependencies Please Add Here ###################
-from flaskr.db import get_contact_form_collection
+"""
+Additional Dependencies Please Add Here
+"""
+from flaskr.model.contact_form_model import get_contact_form_collection
 from flask_mail import Message, Mail
 from flaskr import auth
 
 bp = Blueprint('contact_form', __name__, url_prefix='/contact_form')
-api = Api(bp);
+api = Api(bp)
 
 # take a contact form, send it to seller using mailer
 # Need seller id, buyer id, title, and Message
 # buyer's id can be obtained from session, seller's
 # id has to be passed in by frontend. Frontend can get
 # it from furniture details
+
+
 class Contact(Resource):
     @auth.login_required
     def post(self):
-        pass;
+        pass
 
 # take an id of contact form and delete it
+
+
 class Delete(Resource):
-	@auth.login_required
-	def get(self, contact_form_id):
-		pass;
+    @auth.login_required
+    def get(self, contact_form_id):
+        pass
 
 # take an id return detailed contact form info
+
+
 class Detail(Resource):
     @auth.login_required
     def get(self, contact_form_id):
-        pass;
+        pass
 
-api.add_resource(Contact, '/contact');
-api.add_resource(Delete, '/delete/<string:contact_form_id>');
-api.add_resource(Detail, '/detail/<string:contact_form_id>');
+
+api.add_resource(Contact, '/contact')
+api.add_resource(Delete, '/delete/<string:contact_form_id>')
+api.add_resource(Detail, '/detail/<string:contact_form_id>')
--- source/flask/flaskr/furniture.py
+++ source/flask/flaskr/furniture.py
@@ -1,6 +1,6 @@
 from flask import (
-	Blueprint, flash, g, redirect, render_template, request, session, url_for,
-	jsonify
+    Blueprint, flash, g, redirect, render_template, request, session, url_for,
+    jsonify
 )
 from flask_restful import Api, Resource, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -8,40 +8,49 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import pymongo
 from bson.json_util import dumps
 import json
-
-########### Additional Dependencies Please Add Here ###################
+"""
+Additional Dependencies Please Add Here
+"""
 from flaskr import auth
-from flaskr.db import get_furniture_collection
+from flaskr.model.furniture_model import get_furniture_collection
 
 bp = Blueprint('furniture', __name__, url_prefix='/furniture')
-api = Api(bp);
+api = Api(bp)
 
 # take a form, store information in the database
+
+
 class Post(Resource):
-	@auth.login_required
-	def post(self):
-		pass;
+    @auth.login_required
+    def post(self):
+        pass
 
 # take an id of furniture, delete from database
+
+
 class Delete(Resource):
-	@auth.login_required
-	def get(self, furniture_name):
-		pass;
+    @auth.login_required
+    def get(self, furniture_name):
+        pass
 
 # take revised info, change info in database
+
+
 class Update(Resource):
-	@auth.login_required
-	def post(self):
-		pass;
+    @auth.login_required
+    def post(self):
+        pass
 
 # take an id return furniture info
+
+
 class Detail(Resource):
 	@auth.login_required
 	def get(self, furniture_name):
 		# Get furniture data from database
 		furnitures = get_furniture_collection();
 
-		furniture = furnitures.find_one({'product_name': furniture_name});
+		furniture = furnitures.find_one({'furniture_name': furniture_name});
 		if furniture is None:
 			return jsonify({
 				"status": 319,
@@ -49,23 +58,25 @@ class Detail(Resource):
 				})
 
 		# Get detail from the database
-		product_name = furniture['product_name'];
-		category = furniture['Category'];
+		product_name = furniture['furniture_name'];
+		category = furniture['category'];
 		images = furniture['images'];
-		is_delivery_included = furniture['Is_delivery_included'];
-		price = furniture['Price'];
+		is_delivery_included = furniture['is_delivery_included'];
+		price = furniture['price'];
 		location = furniture['location'];
 		seller_id = furniture['seller'];
+		description = furniture['description'];
 		retJson = {
 			"status": 200,
 			"msg": "Get furniture detail succeeded",
-			'product_name': product_name,
-			'Category': category,
+			'furniture_name': product_name,
+			'category': category,
 			'images': images,
-			'Is_delivery_included': is_delivery_included,
-			'Price': price,
+			'is_delivery_included': is_delivery_included,
+			'price': price,
 			'location': location,
-			'seller': seller_id
+			'seller': seller_id,
+			'description': description
 		}
 
 		return jsonify(retJson);
