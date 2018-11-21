b1858c23cb3891d4950144ffbc38e6dba7b02694
chenghungma <mzh878@live.com>
2018-11-20 17:32:05 -0800

    fix the drawer button bug
    fix the drawer button bug

--- source/react/src/components/NavigationDrawer/NavigationDrawer.js
+++ source/react/src/components/NavigationDrawer/NavigationDrawer.js
@@ -94,8 +94,8 @@ class NavigationDrawer extends React.Component {
 
           {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu"> */}
           {/* Icon button - pop over from right */}
-          <IconButton onClick={this.NavigationDrawer('right', true)} className="test">
-            <MenuIcon />
+          <IconButton onClick={this.NavigationDrawer('right', true)} className="test" color="primary" aria-label="Menu">
+            <MenuIcon/>
           </IconButton>
 
           <Drawer anchor="right" open={this.state.right} onClose={this.NavigationDrawer('right', false)}>
