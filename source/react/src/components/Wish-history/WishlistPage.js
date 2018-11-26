c44a2a775cd6ef071721264481869e8d224dcb95
Shengyao Xiao <shx034@ucsd.edu>
2018-11-26 15:14:08 -0800

    updated wishlist and history logic

--- source/react/src/components/Wish-history/WishlistPage.js
+++ source/react/src/components/Wish-history/WishlistPage.js
@@ -2,17 +2,77 @@ import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
 import Wave from '../common/Wave';
 import Card from '../FurniCard/FurniCard';
+import axios from 'axios';
+import {getLocal} from '../../utils/util';
 
 /* History page and wishlist page share the same css */
 import './WishhistPage.css';
 
 class WishlistPage extends Component {
-  constructor(props){
-    super(props);
-  }
-  submitrequest=()=>{
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {
+                    title: 'furniture', 
+                    id: '1',
+                    img:require('../../static/images/wallpaper1.png'), 
+                    price: '$20',
+                    category: "Electronics"
+                }, 
+                {
+                    title: 'furniture', 
+                    id: '1',
+                    img:require('../../static/images/wallpaper1.png'), 
+                    price: '$20',
+                    category: "Electronics"
+                },
+                {
+                    title: 'furniture', 
+                    id: '1',
+                    img:require('../../static/images/wallpaper1.png'), 
+                    price: '$20',
+                    category: "Electronics"
+                },
+            ]
+        };
+    }
+  
+  /*  componentWillMount() {
+        const token = localStorage.getItem('usertoken');
+        // const decoded = jwt_decode(token);
+        // change the logic later
+        let reqData = {
+            
+        };
+        axios({
+            method: 'get',
+            url: '',
+            withCredentials: false,
+            crossdomain: true,
+            data: reqData,
+            responseType: 'json',
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }).then((response) => {
+            console.log(response.data);
+            let code = response.data.status;
+            if (code === 200) {
+              this.setState({
+                data:response.data 
+              });
+            }
+        }).catch((error) => {
+            console.log("get wishlist: " + error);
+        });
+    
+    }*/
 
-  }
+  /*  handleClick = (id) => {
+        
+    }*/
+
     render() {
         return (
             <div>
@@ -26,35 +86,20 @@ class WishlistPage extends Component {
 
                 {/* cards of furnitures wished,should be from backend*/}
                 <div className="Card-group">
-                <Card
-                    title="Furniture1"
-                    text="First wished furniture"
-                    image={require('../../static/images/wallpaper1.png')}
-                    link="http://localhost:3000/Furniture"
-                />
-
-
-                <Card
-                    title="Furniture2"
-                    text="Second wished furniture"
-                    image={require('../../static/images/wallpaper1.png')}
-                    link="http://localhost:3000/Furniture"
-                />
-
-                <Card
-                    title="Furniture3"
-                    text="Third wished furniture"
-                    image={require('../../static/images/wallpaper1.png')}
-                    link="http://localhost:3000/Furniture"
-                />
-
-                <Card
-                    title="Furniture4"
-                    text="Forth wished furniture"
-                    image={require('../../static/images/wallpaper1.png')}
-                    link="http://localhost:3000/Furniture"
-                />
-                    {/* Should send request for wished furnitures, I DON'T THINK SO */}
+                {
+                    this.state.data.length === 0 
+                    ?
+                    <div>Your wishlist is empty.</div>
+                    :
+                    this.state.data.map(obj=>(
+                        <Card
+                            title={obj.title}
+                            text={obj.price + obj.category}
+                            img={obj.img}
+                            onClick={this.handleClick(obj.id)}
+                        />)
+                    )
+                }
                 </div>
 
             {/* end of  DIV */}
