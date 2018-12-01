a133e257789b0c74ee6fe563d947709669b18530
Wenlin Mao <w6mao@ucsd.edu>
2018-12-01 11:54:15 -0800

    add field for contact form

--- source/react/src/components/message/message.js
+++ source/react/src/components/message/message.js
@@ -2,36 +2,51 @@ import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
 import Wave from '../common/Wave';
 import './message.css';
-import axios from 'axios';
+import axios from 'axios';link
 
 class Mymessage extends Component {
     constructor(props){
         super(props);
         this.state={
-          message:'A request message for you',
+          title: '',
+          buyer_username: '',
+          buyer_email: '',
+          content:'',
           user:'',
-          link:"http://localhost:3000/",
+          link:'',
+          furniture_id:'',
+          furniture_name: "",
         };
       }
     componentWillMount() {
+        const contact_form_id = this.props.location.pathname.substring(8);
+        console.log(contact_form_id)
         const temp= '';
         const token = localStorage.getItem('usertoken');
+
         axios({
             method: 'get',
-            url: 'http://127.0.0.1:5000/contact_form/detail/',
+            url: 'http://127.0.0.1:5000/contact_form/detail/' + contact_form_id,
             withCredentials: false,
             crossdomain: true,
-            // data: reqData,
             responseType: 'json',
             headers: {
                 "Authorization": `Bearer ${token}`
             }
         }).then((response) => {
             console.log(response.data);
+            var data = response.data;
             let code = response.data.status;
             if (code === 200) {
               this.setState({
-                data:response.data
+                title: data.title,
+                buyer_username:  data.buyer_name,
+                buyer_email: data.buyer_email,
+                content: data.content,
+                user:data.buyer_id,
+                link:'',
+                furniture_id: data.furniture,
+                furniture_name: data.furniture_name,
               });
             }
         }).catch((error) => {
