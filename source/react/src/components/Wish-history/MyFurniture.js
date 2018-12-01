d235c7045446482f9fc65e7b0284e2aa591b6089
jiao <jsx19950818@gmail.com>
2018-11-30 21:47:30 -0800

    added delete on myfurniture

--- source/react/src/components/Wish-history/MyFurniture.js
+++ source/react/src/components/Wish-history/MyFurniture.js
@@ -85,9 +85,27 @@ class MyFurniture extends Component {
         }).catch((error) => {
 
         })
+}
 
-
-
+	handledelete(e) {
+	let reqData = {
+        	'furniture_id': this.state.furniture_id,
+	}
+		axios({
+              	method: 'get',
+              	url: 'http://127.0.0.1:5000/user/delete_wishlist',
+              	withCredentials: false,
+              	crossdomain: true,
+              	data: reqData,
+              	responseType: 'json',
+              	headers: {
+                //"Content-Type": "application/x-www-form-urlencoded",
+                "Content-Type": "application/json",
+                "Cache-Control": "no-cache",
+                "Authorization": `Bearer`
+              }
+      	})
+	
 
         // const token = localStorage.getItem('usertoken');
         // axios({
@@ -117,7 +135,7 @@ class MyFurniture extends Component {
         //     console.log("get wishlist error: " + error);
         // });
 
-    }
+   }
 
     render() {
         return (
